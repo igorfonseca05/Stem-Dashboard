2828348f5443cf2f64c6af2fd4e5f0dc53520a5a
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2024-05-02 14:40:50 -0300

    SignUp page finished

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 import './SignUp.css'
@@ -9,6 +9,10 @@ import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom'
 import useMenu from '../../hooks/UseCloseMenu'
 
 function SignUp() {
+
+    const [error, setError] = useState(false)
+    const [success, setSuccess] = useState(false)
+    const [loading, setLoading] = useState(false)
 
     // const {handleMenu} =  useMenu('remove')
     // handleMenu()
@@ -43,10 +47,17 @@ function SignUp() {
                             <input type="password" id="password" name="password" placeholder='Password' required />
 
                             <div className='align-button'>
-                                <button type="submit" className='blue-button'>SIGN UP</button>
+                                {loading && <button type="submit" className='blue-button' style={{opacity: '0.5', cursor: 'not-allowed'}} disabled='true'>SIGN UP</button>}
+
+                                {!loading && <button type="submit" className='blue-button'>SIGN UP</button>}
                             </div>
-                            <p>By clicking Sign Up, I confirm that i am 13 years of age or older and agree to the terms of the <a href='#'>Steam Subscriber Agreement</a> and the <a href='#'>Valve Privacy Policy</a></p>
-                             <p className='infos-container'>Error</p>
+
+                            <p className='info-policy'>By clicking Sign Up, I confirm that i am 13 years of age or older and agree to the terms of the <a href='#'>Steam Subscriber Agreement</a> and the <a href='#'>Valve Privacy Policy</a></p>
+
+                             {error && <div className='infos-container error'><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p></div>}
+
+                             {success && <div className='infos-container success'><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p></div>}
+
                         </form>
                     </div>
                 </div>
