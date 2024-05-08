d51118e217249cebbf32223ec96e0aaefad20530
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2024-05-08 11:45:55 -0300

    adding user authentication

--- src/hooks/useAuthentication.jsx
+++ src/hooks/useAuthentication.jsx
@@ -14,4 +14,22 @@ export function useAuthentication() {
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(null)
     const [success, setSuccess] = useState(null)
+
+    // cleanUp
+    /** deal with memory leak */
+    const [cancelled, setCancelled] =  useState(false)
+
+    const auth = getAuth()
+
+    function checkIfIsCancelled () {
+        if(cancelled) return
+    }
+
+    async function createUser (userData) {
+        useEffect(() => {
+
+        }, [])
+    }
+
+
 }
