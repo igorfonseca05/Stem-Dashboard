8fe7fbff6812ebd8893202f030908852462e4af9
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2024-05-17 17:43:15 -0300

    created the profile page

--- src/pages/ProfilePage/Profile.jsx
+++ src/pages/ProfilePage/Profile.jsx
@@ -11,21 +11,25 @@ function Profile() {
     return (
         <section className='adjust-size'>
             <div className='container-profile'>
-                <figure className='bg-profile'>
-                    {/* <img src="img/destiny.jpg" alt="" /> */}
-                </figure>
-                <div className='photo-profile'>
-                    {user.photoURL ? (<>
-                        <img src={user.PhotoURL} alt="user personal image" />
-                    </>) : (<>
-                        <div className='profile-image-letter'>
-                            <h2>{user.displayName?.slice(0, 1)}</h2>
+                <div className='infos-profile'>
+                    <figure className='bg-profile'>
+                        <img src="img/destiny.jpg" alt="" />
+                    </figure>
+                    <div className='infos-profile-form'>
+                        <div className='photo-profile'>
+                            {user.photoURL ? (<>
+                                <img src={user.PhotoURL} alt="user personal image" />
+                            </>) : (<>
+                                <div className='profile-image-letter'>
+                                    <h2>{user.displayName?.slice(0, 1)}</h2>
+                                </div>
+                            </>)}
                         </div>
-                    </>)}
+                        <h2>{user.displayName}</h2>
+                    </div>
                 </div>
             </div>
-
-            <img src="" alt="" /></section>
+        </section>
     )
 
 }
