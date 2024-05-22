5dc62a39424a1f2b559441acdad32b6ec938ded6
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2024-05-22 08:24:48 -0300

    creating profile page

--- src/pages/ProfilePage/Profile.jsx
+++ src/pages/ProfilePage/Profile.jsx
@@ -15,36 +15,45 @@ function Profile() {
     // console.log(date.toLocaleDateString())
 
     return (
-        <section className='adjust-size'>
-            <div className='container-profile'>
-                <div className='infos-profile'>
-                    <figure className='bg-profile'>
-                        <img src="img/call.jpg" alt="" />
-                    </figure>
-                    <div className='infos-profile-form'>
-                        {/* <div className='photo-profile'>
-                            {user.photoURL ? (<>
-                                <img src={user.PhotoURL} alt="user personal image" />
-                            </>) : (<>
-                                <div className='profile-image-letter'>
-                                    <h2>{user.displayName?.slice(0, 1)}</h2>
-                                </div>
-                            </>)}
-                        </div> */}
-                        <div className='photo-profile'>
-                                <img src='https://play-lh.googleusercontent.com/xXqGEr9xcJj6E5_Q_IKCp9N7fhBPlq6XAzU8WvKa_UT1psSWgXQXOJyZbn-mOsKcJcYQ' alt="user personal image" />
-                                <div className='online-marker'></div>
-                        </div>
-                       <div className='infos-user'>
-                       <h2>{user.displayName}</h2>
-                       <p className='email'>{user.email}</p>
-                       <p className='bios'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. r adipisicing elitr lor lsdfgsd</p>
-                       <p className='createdAt'>{`Conta criada em: ${date.toLocaleDateString()}`}</p>
-                       </div>
-                    </div>
-                </div>
+        // <section className='adjust-size'>
+        //     <div className='container-profile'>
+        //         <div className='infos-profile'>
+        //             <figure className='bg-profile'>
+        //                 <img src="img/call.jpg" alt="" />
+        //             </figure>
+        //             <div className='infos-profile-form'>
+        //                 {/* <div className='photo-profile'>
+        //                     {user.photoURL ? (<>
+        //                         <img src={user.PhotoURL} alt="user personal image" />
+        //                     </>) : (<>
+        //                         <div className='profile-image-letter'>
+        //                             <h2>{user.displayName?.slice(0, 1)}</h2>
+        //                         </div>
+        //                     </>)}
+        //                 </div> */}
+        //                 <div className='photo-profile'>
+        //                         <img src='https://play-lh.googleusercontent.com/xXqGEr9xcJj6E5_Q_IKCp9N7fhBPlq6XAzU8WvKa_UT1psSWgXQXOJyZbn-mOsKcJcYQ' alt="user personal image" />
+        //                         <div className='online-marker'></div>
+        //                 </div>
+        //                <div className='infos-user'>
+        //                <h2>{user.displayName}</h2>
+        //                <p className='email'>{user.email}</p>
+        //                <p className='bios'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. r adipisicing elitr lor lsdfgsd</p>
+        //                <p className='createdAt'>{`Conta criada em: ${date.toLocaleDateString()}`}</p>
+        //                </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </section>
+
+        <div className='grid adjust-size'>
+            <div className='children'>1</div>
+            <div className='children'>
+                <div className='content'>A</div>
+                <div className='content'>B</div>
+                <div className='content'>C</div>
             </div>
-        </section>
+        </div>
     )
 
 }
