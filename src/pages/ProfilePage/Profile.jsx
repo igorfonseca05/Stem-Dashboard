951f3b082a163b27b10d698c4c9af65642d33494
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2024-05-30 09:14:21 -0300

    solved localstorage problem

--- src/pages/ProfilePage/Profile.jsx
+++ src/pages/ProfilePage/Profile.jsx
@@ -14,7 +14,7 @@ function Profile() {
     const location = useLocation()
 
     const { handleMenu, isOpen } = useMenu(location.pathname)
-     handleMenu()
+    handleMenu()
 
     //  useEffect(() => {
     //     document.querySelector('.nav-container').style.display = 'block'
@@ -26,14 +26,23 @@ function Profile() {
     // console.log(user.reloadUserInfo.createdAt)
 
     const stringData = parseInt(user.reloadUserInfo.createdAt)
-    const date = new Date(stringData)
+    const date = new Date(stringData).toLocaleDateString()
 
     // console.log(date.toLocaleDateString())
 
     return (
         <section className='adjust-size profile-container'>
             <div className='content-profile'>
-                <div className='user-info'>1</div>
+                <div className='user-info'>
+                    <div className='user-info-content'>
+                        <figure>
+                            <img src="https://png.pngtree.com/thumb_back/fh260/background/20230610/pngtree-the-character-wearing-headphones-with-an-ear-piercing-in-his-head-image_2919756.jpg" alt="" />
+                        </figure>
+                        <h3>{user.displayName}</h3>
+                        <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ab pariatur beatae expedita deserunt magnam voluptatum?</p>
+                        <p>Conta criada em: {date}</p>
+                    </div>
+                </div>
                 <figure className='bg-profile'>
                     <img src="img/call.jpg" alt="" />
                 </figure>
@@ -42,7 +51,7 @@ function Profile() {
             </div>
         </section>
 
-       
+
     )
 
 }
